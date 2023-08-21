f3d23832f939f68b907bc34ed03063d7eb28189c
Yeite Rilsosing Koireng <53212931+chimmykk@users.noreply.github.com>
2023-08-21 17:05:02 +0530

    update replyautoreadme

---
+++ replyautomatically/replyauto.py
@@ -0,0 +1,10 @@
+#updating the code here
+
+#STEPS involved
+
+#automate by script
+# Read/Stream tweets from elon real -time upon receiving latest tweets
+# extract and input the extracted on the AI doge character
+# run concurrently the thread of the application and Bandicam/OBS
+# save it on a folder /[path], and auto-delete after 5 min
+# upload / reply back the user(elon)
